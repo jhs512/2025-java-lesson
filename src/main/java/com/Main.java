bcddc4c2cb53a68465a0473fbc91bcb050d7168f
jhs512 <jangka512@gmail.com>
2026-01-22 01:28:55 +0900

    0003

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,5 +1,15 @@
 void main() {
-    IO.println("Hello");
+    int x;
 
-    IO.println("World");
+    x = 5;
+
+    IO.println(x + 10);
+
+    x = 10;
+
+    IO.println(x + 10);
+
+    x = x + 20;
+
+    IO.println(x + 10);
 }
